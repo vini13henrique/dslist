10c25b2a69423ef8c9d1416bbc542ac93ea98053
vini13henrique <vinicius13henrique01@gmail.com>
2025-08-25 00:26:09 -0300

    Get game by id

--- src/main/java/com/vinicius/dslist/services/GameService.java
+++ src/main/java/com/vinicius/dslist/services/GameService.java
@@ -5,8 +5,10 @@ import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.vinicius.dslist.dto.GameDto;
 import com.vinicius.dslist.dto.GameMinDto;
 import com.vinicius.dslist.entities.Game;
 import com.vinicius.dslist.repository.GameRepository;
@@ -17,7 +19,12 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 
-	
+	@Transactional(readOnly = true)
+	public GameDto findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDto(result);
+	}
+	@Transactional(readOnly = true)
 	public List<GameMinDto> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDto> dto = result.stream().map(x -> new GameMinDto(x)).toList();
